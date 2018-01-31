35fe7221f4c63048d2fe8f6673e1e962310e6995
Sviridov <Svyrydovartem@gmail.com>
2018-01-31 19:13:30 +0200

    New finger data

--- Diplom/Models/Finger.cs
+++ Diplom/Models/Finger.cs
@@ -42,29 +42,25 @@ namespace Diplom.Models
                         new Sector
                         {
                             Angl1 = 52, Angl2 = 72,
-                            Name = "Лоб виска",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 72, Angl2 = 85,
                             Name = "Глаз",
                         },
                         new Sector
                         {
-                            Angl1 = 85, Angl2 = 100,
+                            Angl1 = 72, Angl2 = 85,
                             Name = "Нос, ухо",
                         },
                         new Sector
                         {
-                            Angl1 = 100, Angl2 = 150,
-                            Name = "Чулюсть, зубы",
+                            Angl1 = 85, Angl2 = 100,
+                            Name = "Зубы",
                         },
                         new Sector
                         {
-                            Angl1 = 150, Angl2 = 210,
-                            Name = "Зона шеи, щитовидная железа",
+                            Angl1 = 100, Angl2 = 240,
+                            Name = "Зона горла, щитовидная железа",
                         },
                         
+
                     }
                 },
                 new Finger
@@ -74,196 +70,186 @@ namespace Diplom.Models
                     {
                         new Sector
                         {
-                            Angl1 = 310, Angl2 = 60,
+                            Angl1 = 0, Angl2 = 60,
                             Name = "Поперечно ободочная кишка",
                         },
                         new Sector
                         {
-                            Angl1 = 60, Angl2 = 80,
-                            Name = "Шейный отдел",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 80, Angl2 = 97,
-                            Name = "Восходящая кишка",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 280, Angl2 = 310,
+                            Angl1 = 60, Angl2 = 120,
                             Name = "Нисходящая кишка",
                         },
                         new Sector
                         {
-                            Angl1 = 80, Angl2 = 97,
-                            Name = "Грудной отдел",
+                            Angl1 = 120, Angl2 = 150,
+                            Name = "Сигмовидная кишка",
                         },
                         new Sector
                         {
-                            Angl1 = 210, Angl2 = 230,
+                            Angl1 = 150, Angl2 = 180,
                             Name = "Прямая кишка",
                         },
                         new Sector
                         {
-                            Angl1 = 160, Angl2 = 170,
+                            Angl1 = 180, Angl2 = 230,
+                            Name = "Копчик крестец",
+                        },
+                        new Sector
+                        {
+                            Angl1 = 230, Angl2 = 265,
                             Name = "Поясничный отдел",
                         },
                         new Sector
                         {
-                            Angl1 = 130, Angl2 = 160,
-                            Name = "Аппендикс",
+                            Angl1 = 265, Angl2 = 310,
+                            Name = "Грудной отдел",
                         },
                         new Sector
                         {
-                            Angl1 = 190, Angl2 = 210,
-                            Name = "Крестец",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 170, Angl2 = 190,
-                            Name = "Копчик",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 160, Angl2 = 170,
-                            Name = "Слепая кишка",
+                            Angl1 = 310, Angl2 = 325,
+                            Name = "Шейный отдел",
                         }
+                        
                     }
                 },
-                 new Finger
+                new Finger
                 {
                     Name = "Средний палец",
                     Sectors = new List<Sector>
                     {
                         new Sector
                         {
-                            Angl1 = 320, Angl2 = 45,
-                            Name = "Зона головы",
+                            Angl1 = 335, Angl2 = 25,
+                            Name = "Печень",
                         },
                         new Sector
                         {
-                            Angl1 = 270, Angl2 = 45,
-                            Name = "Циркуляция крови",
+                            Angl1 = 25, Angl2 = 30,
+                            Name = "Грудь",
                         },
                         new Sector
                         {
-                            Angl1 = 45, Angl2 = 80,
-                            Name = "Грудной отдел",
+                            Angl1 = 30, Angl2 = 70,
+                            Name = "Живот",
                         },
                         new Sector
                         {
-                            Angl1 = 80, Angl2 = 110,
-                            Name = "Лимфа",
+                            Angl1 = 70, Angl2 = 90,
+                            Name = "Таз",
                         },
                         new Sector
                         {
-                            Angl1 = 210, Angl2 = 270,
-                            Name = "Левая сторона груди, сердце",
+                            Angl1 = 90, Angl2 = 140,
+                            Name = "Ноги",
                         },
                         new Sector
                         {
-                            Angl1 = 110, Angl2 = 160,
-                            Name = "Зона живота",
+                            Angl1 = 140, Angl2 = 158,
+                            Name = "Стопа",
                         },
                         new Sector
                         {
-                            Angl1 = 180, Angl2 = 210,
+                            Angl1 = 158, Angl2 = 205,
                             Name = "Почки",
                         },
                         new Sector
                         {
-                            Angl1 = 160, Angl2 = 180,
-                            Name = "Печень",
+                            Angl1 = 205, Angl2 = 335,
+                            Name = "Кровь",
                         },
                     }
                 },
-                 new Finger
+                new Finger
                 {
                     Name = "Безымянный палец",
                     Sectors = new List<Sector>
                     {
                         new Sector
                         {
-                            Angl1 = 360, Angl2 = 80,
+                            Angl1 = 350, Angl2 = 25,
                             Name = "Эпифиз",
                         },
                         new Sector
                         {
-                            Angl1 = 310, Angl2 = 360,
-                            Name = "Гипофиз",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 280, Angl2 = 310,
+                            Angl1 = 25, Angl2 = 40,
                             Name = "Гипоталамус",
                         },
                         new Sector
                         {
-                            Angl1 = 260, Angl2 = 280,
+                            Angl1 = 40, Angl2 = 155,
                             Name = "Нервная система",
                         },
                         new Sector
                         {
-                            Angl1 = 80, Angl2 = 110,
-                            Name = "Эндокринная система",
+                            Angl1 = 155, Angl2 = 210,
+                            Name = "Матка, простата",
                         },
                         new Sector
                         {
-                            Angl1 = 250, Angl2 = 260,
-                            Name = "Селезенка",
+                            Angl1 = 210, Angl2 = 230,
+                            Name = "Яичник",
                         },
                         new Sector
                         {
-                            Angl1 = 110, Angl2 = 130,
-                            Name = "Поджелудочная",
-                        },
-                        new Sector
-                        {
-                            Angl1 = 230, Angl2 = 250,
+                            Angl1 = 230, Angl2 = 245,
                             Name = "Надпочечник",
                         },
                         new Sector
                         {
-                            Angl1 = 160, Angl2 = 230,
-                            Name = "Уро-генитальная система",
+                            Angl1 = 245, Angl2 = 255,
+                            Name = "П/жел железа",
+                        },
+                        new Sector
+                        {
+                            Angl1 = 255, Angl2 = 280,
+                            Name = "Вилочковая железа",
+                        },
+                        new Sector
+                        {
+                            Angl1 = 280, Angl2 = 315,
+                            Name = "Паращитовидная железа",
+                        },
+                        new Sector
+                        {
+                            Angl1 = 315, Angl2 = 350,
+                            Name = "Паращитовидная железа",
                         },
                     }
                 },
-                 new Finger
+                new Finger
                 {
                     Name = "Мизинец",
                     Sectors = new List<Sector>
                     {
                         new Sector
                         {
-                            Angl1 = 280, Angl2 = 45,
-                            Name = "Коронарные сосуды",
+                            Angl1 = 0, Angl2 = 90,
+                            Name = "Сосуды сердца, левое предсердие",
                         },
                         new Sector
                         {
-                            Angl1 = 45, Angl2 = 90,
-                            Name = "Двенадцатиперстная кишка",
+                            Angl1 = 90, Angl2 = 150,
+                            Name = "Левый желудочек, коронарные сосуды",
                         },
                         new Sector
                         {
-                            Angl1 = 90, Angl2 = 120,
-                            Name = "Тонкий кишечник",
+                            Angl1 = 150, Angl2 = 210,
+                            Name = "Бронхи, Легкие, молочные железы",
                         },
                         new Sector
                         {
-                            Angl1 = 210, Angl2 = 240,
-                            Name = "Тощая кишка",
+                            Angl1 = 210, Angl2 = 308,
+                            Name = "Подвздошная кишка",
                         },
                         new Sector
                         {
-                            Angl1 = 120, Angl2 = 210,
-                            Name = "Респираторная система",
+                            Angl1 = 308, Angl2 = 0,
+                            Name = "Valva ileocaccalis",
                         },
                     }
                 },
                 
                 #endregion Left hand
-
-
+                
                 #region Right hand
               
                 #endregion Right hand 
